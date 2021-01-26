018cb1753103f6d711fbd0e71a3a849509fe3d7c
partum <jarringt@uccs.edu>
2021-01-26 13:56:13 -0700

    started timer

--- script.js
+++ script.js
@@ -41,10 +41,57 @@ function addEntry(){
     var time = document.querySelector('input[type="time"]').value; //for some reason this works but not getElement by ID
     var timeText = document.createTextNode("Last taken: " + time);
     newtime.append(timeText);
-    document.getElementById("entries").lastElementChild.appendChild(newtime); //this conflicts with the first append for no reason
-    //document.getElementById("entries").lastElementChild.appendChild(document.createElement("p")); //test
+    document.getElementById("entries").lastElementChild.appendChild(newtime); 
+    //get the wait time and calculate next dosage time
+    var elem = document.createElement("time");
+    var hours = document.getElementById("input-number").value; 
+    var addTime = parseInt(time.slice(0,2)) + parseInt(hours); 
+    time = addTime + time.slice(2,5);
+    var node = document.createTextNode("Next dosage at " + time);
+    elem.append(node);
+    document.getElementById("entries").lastElementChild.appendChild(elem);
+    //create count down
+    var elem = document.createElement("time");
+    elem.setAttribute("id", med)
+    var node = document.createTextNode(hours+":00:00");
+    elem.append(node);
+    document.getElementById("entries").lastElementChild.appendChild(elem);
+    //timer
+    miliTime = hours * 60 * 60 * 1000; 
+    //how can I give each entry a unique timer
+    //how can I make the time display as 5:04 and not 5:4
+    setInterval(function(){
+        var hh = Math.floor((miliTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var mm = Math.floor((miliTime % (1000 * 60 * 60)) / (1000 * 60));
+        var ss = Math.floor((miliTime % (1000 * 60)) / 1000);
+        document.getElementById(med).innerHTML = hh + ":" + mm + ":" + ss;
+        miliTime -= 1000;
+    }, 1000);
     //get rid of form and bring back add button
     document.getElementById("add").style.visibility = "visible";
     document.getElementById("addForm").style.visibility = "hidden"; 
+    
+    
 }
-//<input type="text" id="task" name="task" placeholder="Enter your task here and check the boxes to prioritize." required>
+
+function countDown(miliTime){
+    var hh = Math.floor((miliTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var mm = Math.floor((miliTime % (1000 * 60 * 60)) / (1000 * 60));
+    var ss = Math.floor((miliTime % (1000 * 60)) / 1000);
+    miliTime = miliTime - 1000; //issue here
+    return miliTime;
+}
+
+//test
+function test(){
+    console.log("hi");
+    timeSec = 10;
+}
+//setInterval(countDown, 1000);
+
+//make a timer
+
+// setInterval(function(){
+//     console.log(timeSec)
+//     timeSec -= 1;
+// },1000)
